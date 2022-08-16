a42398f4e9df9fd6edc8d8fbe563fff50b88f5d1
du <truongdu.td88@gmail.com>
2022-08-16 17:24:06 +0700

    css Header

--- src/components/Layouts/components/Header/index.js
+++ src/components/Layouts/components/Header/index.js
@@ -1,5 +1,35 @@
+import classNames from 'classnames/bind';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark } from '@fortawesome/free-solid-svg-icons';
+
+import styles from './Header.module.scss';
+import images from '../../../../assets/images';
+
+const cx = classNames.bind(styles);
+
 function Header() {
-    return <h2>header</h2>;
+    return (
+        <header className={cx('header')}>
+            <div className={cx('inner')}>
+                <div className={cx('header-box')}>
+                    <h1 className={cx('header-logo')}>
+                        <img src={images.logo} alt="tiktok" />
+                    </h1>
+                    <div className={cx('header-search')}>
+                        <input type="text" placeholder="Search accounts and videos" spellCheck={false} />
+                        <button className={cx('header-search-clear')}>
+                            <FontAwesomeIcon icon={faCircleXmark}></FontAwesomeIcon>
+                        </button>
+                        <button className={cx('header-search-btn')}></button>
+                    </div>
+                    <div className={cx('header-login')}>
+                        <button>Upload</button>
+                        <button>Login</button>
+                    </div>
+                </div>
+            </div>
+        </header>
+    );
 }
 
 export default Header;
